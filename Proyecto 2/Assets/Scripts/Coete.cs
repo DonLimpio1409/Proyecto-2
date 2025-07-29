e63bbcee92c237e3acb20fc7d9b4a6af11058148
DonLimpio1409 <ppn1002@alu.ubu.es>
2025-07-29 15:43:16 +0200

    Disparo perfeccionado con particulas

--- Proyecto 2/Assets/Scripts/Coete.cs
+++ Proyecto 2/Assets/Scripts/Coete.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class Coete : MonoBehaviour
 {
     [SerializeField] float velocidad = 5f;
-    
+    [SerializeField] GameObject explosion;
+    Vector3 direccion;
 
     void Start()
     {
+        //Creacion del punto que define el centro de la pantalla
         Vector3 centroPantalla = new Vector3(Screen.width / 2f, Screen.height / 2f, 10f);
         Vector3 puntoMundo = Camera.main.ScreenToWorldPoint(centroPantalla);
-        Vector3 direccion = (puntoMundo - transform.position).normalized;
+        direccion = (puntoMundo - transform.position).normalized;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        //Avanzar constantemente
         transform.position += direccion * velocidad * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //Destruccion y generacion de particulas
+        Vector3 puntoImpacto = other.contacts[0].point;
+        vector3 normal = puntoImpacto.point;
+        Quaternion posicionExplosion = Quaternion.LookRotation(-normal);
+
+        Instantiate(explosion, puntoImpacto, posicionExplosion);
+        Destroy(gameObject);
     }
 }
